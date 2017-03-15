06235e669e169df6bbea00263c63b956169f541b
Alan deLevie <adelevie@gmail.com>
2017-03-15 15:59:18 -0400

    Adding simple script to deploy local version

---
+++ bin/deploy-local-branch.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cp .env.test .env
+docker-compose build
+./bin/predeploy.sh
+./bin/deploy.sh dev
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,11 +16,19 @@ if [ $SPACE = 'production' ]; then
   API_MANIFEST="manifest-api.yml"
   FRONTEND_NAME="eqip-prototype"
   FRONTEND_MANIFEST="manifest-frontend.yml"
+  INTERACTIVE_LOGIN=0
 elif [ $SPACE = 'staging' ]; then
   API_NAME="eqip-prototype-api-staging"
   API_MANIFEST="manifest-api-staging.yml"
   FRONTEND_NAME="eqip-prototype-staging"
   FRONTEND_MANIFEST="manifest-frontend-staging.yml"
+  INTERACTIVE_LOGIN=0
+elif [ $SPACE = 'dev' ]; then
+  API_NAME="eqip-prototype-api-dev"
+  API_MANIFEST="manifest-api-dev.yml"
+  FRONTEND_NAME="eqip-prototype-dev"
+  FRONTEND_MANIFEST="manifest-frontend-dev.yml"
+  INTERACTIVE_LOGIN=1
 else
   echo "Unknown space: $SPACE"
   exit
@@ -37,6 +45,7 @@ docker pull adelevie/cf-cli:latest
 # so we're using this function instead
 cf_run() {
   docker run \
+    -it \
     --rm \
     -v $HOME/.cf:/root/.cf \
     -v $PWD:/app \
@@ -44,6 +53,20 @@ cf_run() {
     cf "$@"
 }
 
-cf_run login -a $API -u $CF_USERNAME -p $CF_PASSWORD -o $ORG -s $SPACE
+if [ $INTERACTIVE_LOGIN = 1 ]; then
+  cf_run login \
+    -a api.fr.cloud.gov \
+    --sso \
+    -s $SPACE \
+    -o $ORG
+else
+  cf_run login \
+    -a $API \
+    -u $CF_USERNAME \
+    -p $CF_PASSWORD \
+    -o $ORG \
+    -s $SPACE
+fi
+
 cf_run zero-downtime-push $API_NAME -f $API_MANIFEST
 cf_run zero-downtime-push $FRONTEND_NAME -f $FRONTEND_MANIFEST
